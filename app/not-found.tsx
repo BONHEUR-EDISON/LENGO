7dff60dbab7e3f81f29846ecc08c950d625d1dfb
BONHEUR-EDISON <bonheurluundoedison@gmail.com>
2026-03-11 14:49:53 +0100

    first commit6

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center flex-col gap-4">
+      <h1 className="text-4xl font-bold">404</h1>
+      <p>Page introuvable</p>
+    </div>
+  );
+}
